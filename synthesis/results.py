4d968ec161d8619debdf2a258ad5ffe8e3d37313
Andrea Franchini <hello@andreafranchini.com>
2023-01-15 12:17:48 +0100

    move results printer to results.py

--- synthesis/results.py
+++ synthesis/results.py
@@ -1,14 +1,71 @@
-from z3 import ArithRef
+from termcolor import colored, cprint
+
+from synthesis.types import State
 
 
-def parse_synthesis_result(res):
-    if isinstance(res, ArithRef):
-        # if it's more than one element
-        if str(res.arg(0)).startswith('Or('):  # UGLY
-            elems = [c.arg(1) for c in res.arg(0).children()]
-            value = res.arg(1)
-            return elems, value.as_long()
-        else:
-            elem = res.arg(0).arg(1)
-            value = res.arg(1)
-            return [elem], value.as_long()
+def check_synth_results(state: State):
+    # Make sure we have a model!
+    state.solver.check()
+    model = state.solver.model()
+
+    # Quick testing of synthetized values
+
+    print('Values of \'vm1\' that have an associated value (cpu_count)')
+    vm1 = state.data.Elems['elem_139682454814288']
+    vm1_cpu_count = model.eval(
+        state.rels.int.AttrValueRel(
+            vm1.ref,
+            state.data.Attrs['infrastructure_ComputingNode::cpu_count'].ref)).as_long()
+    print(vm1.name, vm1_cpu_count)
+    assert vm1_cpu_count > 4
+
+    # A value that is not assigned by anything is equal to 2???
+    print('Attribute of \'vm1\' that does not have an associated value (e.g. cost)')
+    vm_cost_value = model.eval(state.rels.int.AttrValueRel(
+        vm1.ref, state.data.Attrs['infrastructure_ComputingNode::cost'].ref))
+    print(vm_cost_value)
+
+    # Works only if we have unbound elems!
+
+    # For each unbound variable, print the assigned values for each attribute (of type Integer)
+    print("\nSynthesis Results: synthetized results have a 'True' at the end of the line")
+    for elem_k, elem_v in state.data.Elems.items():
+        elem_class = str(model.eval(state.rels.ElemClass(elem_v.ref)))
+        cprint(f'{elem_k}\t{elem_class}', "magenta")
+
+        evaluate_associations(state, model, elem_v, elem_class)
+        evaluate_attributes(state, model, elem_v, elem_class)
+
+    return state
+
+
+def evaluate_associations(state, model, elem_v, elem_class):
+    for elem_dest_k, elem_dest_v in state.data.Elems.items():
+        e1 = elem_v.ref
+        e2 = elem_dest_v.ref
+        for assoc_k, _ in state.data.Classes[elem_class].associations.items():
+            assoc = state.data.Assocs[assoc_k].ref
+            if model.eval(state.rels.AssocRel(e1, assoc, e2)):
+                if elem_dest_v.unbound:
+                    elem_dest_k = colored(
+                        elem_dest_k, on_color="on_yellow")
+                _class, _assoc = assoc_k.split("::")
+                _assoc = colored(_assoc, "blue")
+                elem_dest_k = colored(elem_dest_k, "yellow")
+
+                print(f'\t\t\t{_class}::{_assoc}\t{elem_dest_k}')
+
+
+def evaluate_attributes(state, model, elem_v, elem_class):
+    for elem_attr_k, elem_attr_v in state.data.Classes[elem_class].attributes.items():
+        if elem_attr_v['type'] == 'Integer':
+            value = str(model.eval(state.rels.int.AttrValueRel(
+                elem_v.ref, state.data.Attrs[elem_attr_k].ref)))
+            synthetized = bool(str(model.eval(state.rels.int.AttrSynthRel(
+                elem_v.ref, state.data.Attrs[elem_attr_k].ref))) == "True")
+            synthetized = colored(
+                synthetized, "green" if synthetized else "red")
+            _class, _attr = elem_attr_k.split("::")
+            _attr = colored(_attr, "cyan")
+            value = colored(value, "yellow")
+            print(f'\t\t\t{_class}::{_attr}\t{value}\t{synthetized}')
--- synthesis/synthesis.py
+++ synthesis/synthesis.py
@@ -1,7 +1,7 @@
 from z3 import *
 
 from synthesis.requirements import init_requirements
-from termcolor import colored, cprint
+from synthesis.results import check_synth_results
 from synthesis.setup import setup
 from synthesis.solver import solve
 from synthesis.tests import run_tests
@@ -30,71 +30,3 @@ def main():
     ).apply(
         check_synth_results
     )
-
-
-def check_synth_results(state: State):
-    # Make sure we have a model!
-    state.solver.check()
-    model = state.solver.model()
-
-    # Quick testing of synthetized values
-
-    print('Values of \'vm1\' that have an associated value (cpu_count)')
-    vm1 = state.data.Elems['elem_139682454814288']
-    vm1_cpu_count = model.eval(
-        state.rels.int.AttrValueRel(
-            vm1.ref,
-            state.data.Attrs['infrastructure_ComputingNode::cpu_count'].ref)).as_long()
-    print(vm1.name, vm1_cpu_count)
-    assert vm1_cpu_count > 4
-
-    # A value that is not assigned by anything is equal to 2???
-    print('Attribute of \'vm1\' that does not have an associated value (e.g. cost)')
-    vm_cost_value = model.eval(state.rels.int.AttrValueRel(
-        vm1.ref, state.data.Attrs['infrastructure_ComputingNode::cost'].ref))
-    print(vm_cost_value)
-
-    # Works only if we have unbound elems!
-
-    # For each unbound variable, print the assigned values for each attribute (of type Integer)
-    print("\nSynthesis Results: synthetized results have a 'True' at the end of the line")
-    for elem_k, elem_v in state.data.Elems.items():
-        elem_class = str(model.eval(state.rels.ElemClass(elem_v.ref)))
-        cprint(f'{elem_k}\t{elem_class}', "magenta")
-
-        evaluate_associations(state, model, elem_v, elem_class)
-        evaluate_attributes(state, model, elem_v, elem_class)
-
-    return state
-
-
-def evaluate_associations(state, model, elem_v, elem_class):
-    for elem_dest_k, elem_dest_v in state.data.Elems.items():
-        e1 = elem_v.ref
-        e2 = elem_dest_v.ref
-        for assoc_k, _ in state.data.Classes[elem_class].associations.items():
-            assoc = state.data.Assocs[assoc_k].ref
-            if model.eval(state.rels.AssocRel(e1, assoc, e2)):
-                if elem_dest_v.unbound:
-                    elem_dest_k = colored(
-                        elem_dest_k, on_color="on_yellow")
-                _class, _assoc = assoc_k.split("::")
-                _assoc = colored(_assoc, "blue")
-                elem_dest_k = colored(elem_dest_k, "yellow")
-
-                print(f'\t\t\t{_class}::{_assoc}\t{elem_dest_k}')
-
-
-def evaluate_attributes(state, model, elem_v, elem_class):
-    for elem_attr_k, elem_attr_v in state.data.Classes[elem_class].attributes.items():
-        if elem_attr_v['type'] == 'Integer':
-            value = str(model.eval(state.rels.int.AttrValueRel(
-                elem_v.ref, state.data.Attrs[elem_attr_k].ref)))
-            synthetized = bool(str(model.eval(state.rels.int.AttrSynthRel(
-                elem_v.ref, state.data.Attrs[elem_attr_k].ref))) == "True")
-            synthetized = colored(
-                synthetized, "green" if synthetized else "red")
-            _class, _attr = elem_attr_k.split("::")
-            _attr = colored(_attr, "cyan")
-            value = colored(value, "yellow")
-            print(f'\t\t\t{_class}::{_attr}\t{value}\t{synthetized}')
